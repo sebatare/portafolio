16157964556dc05458ef8463ba7904877502d136
Sebastian Desde Windows <sebatapiareb@gmail.com>
2025-07-06 23:44:52 -0400

    minimal errors

--- frontend/src/components/ListaProyectos.tsx
+++ frontend/src/components/ListaProyectos.tsx
@@ -73,7 +73,7 @@ const ListaProyectos = () => {
           animate="visible"
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
         >
-          {projects.map((project, index) => (
+          {projects.map((project) => (
             <motion.div
               key={project.id}
               variants={itemVariants}
